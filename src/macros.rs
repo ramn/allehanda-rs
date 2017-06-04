9086d53929cdbc8c2e43458de383e67356666c3d
ramn <github@rymdimperiet.org>
2017-06-04 18:59:50 +0200

    Add import in macro

--- src/macros.rs
+++ src/macros.rs
@@ -1,9 +1,11 @@
 #[macro_export]
 macro_rules! eprintln {
     ($fmt:expr) => {
+        use std::io::Write;
         writeln!(std::io::stderr(), $fmt).unwrap();
     };
     ($fmt:expr, $($arg:tt)*) => {
+        use std::io::Write;
         writeln!(std::io::stderr(), $fmt, $( $arg )*).unwrap();
     };
 }
