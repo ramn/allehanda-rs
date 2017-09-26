acf1d5e7c6f7adebc939fbfdc8bed014db3d3a5b
ramn <github@rymdimperiet.org>
2017-09-26 23:57:45 +0200

    Add non-mutating permutation function

--- src/permutation.rs
+++ src/permutation.rs
@@ -1,4 +1,4 @@
-/// Finds next lexicographical permutation
+/// Mutates into lexicographical permutation
 pub fn permute(xs: &mut [usize]) {
     let i = xs.iter().zip(xs.iter().skip(1)).enumerate()
         .fold(0, |memo, (i, (a, b))| if a < b { i } else { memo });
@@ -8,41 +8,57 @@ pub fn permute(xs: &mut [usize]) {
     xs[i+1..].reverse();
 }
 
+/// Returns next lexicographical permutation
+pub fn next_permutation(xs: &[usize]) -> Vec<usize> {
+    let mut xs: Vec<_> = xs.to_vec();
+    permute(&mut xs);
+    xs
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const EXPECTED_LIST: [[usize; 4]; 24] = [
+        [1,2,3,4],
+        [1,2,4,3],
+        [1,3,2,4],
+        [1,3,4,2],
+        [1,4,2,3],
+        [1,4,3,2],
+        [2,1,3,4],
+        [2,1,4,3],
+        [2,3,1,4],
+        [2,3,4,1],
+        [2,4,1,3],
+        [2,4,3,1],
+        [3,1,2,4],
+        [3,1,4,2],
+        [3,2,1,4],
+        [3,2,4,1],
+        [3,4,1,2],
+        [3,4,2,1],
+        [4,1,2,3],
+        [4,1,3,2],
+        [4,2,1,3],
+        [4,2,3,1],
+        [4,3,1,2],
+        [4,3,2,1],
+        ];
+
     #[test]
     fn test_permute() {
         let mut xs = [1, 2, 3, 4];
-        let expected_list = [
-            [1,2,3,4],
-            [1,2,4,3],
-            [1,3,2,4],
-            [1,3,4,2],
-            [1,4,2,3],
-            [1,4,3,2],
-            [2,1,3,4],
-            [2,1,4,3],
-            [2,3,1,4],
-            [2,3,4,1],
-            [2,4,1,3],
-            [2,4,3,1],
-            [3,1,2,4],
-            [3,1,4,2],
-            [3,2,1,4],
-            [3,2,4,1],
-            [3,4,1,2],
-            [3,4,2,1],
-            [4,1,2,3],
-            [4,1,3,2],
-            [4,2,1,3],
-            [4,2,3,1],
-            [4,3,1,2],
-            [4,3,2,1],
-            ];
-        for expected in expected_list.iter().skip(1) {
+        for expected in EXPECTED_LIST.iter().skip(1) {
             permute(&mut xs);
             assert_eq!(*expected, xs);
+        }
+    }
+
+    #[test]
+    fn test_next_permutation() {
+        for (a, b) in EXPECTED_LIST.iter().zip(EXPECTED_LIST.iter().skip(1)) {
+            assert_eq!(next_permutation(a), b.to_vec());
         }
     }
 }
